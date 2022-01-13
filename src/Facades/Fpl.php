2a8ca9b2ba47d120f25aec65d026e313f28e770e
Njazi Shehu <shehu.nj@gmail.com>
2022-01-13 22:40:40 +0100

    Added methods for static analyzers

--- src/Facades/Fpl.php
+++ src/Facades/Fpl.php
@@ -2,11 +2,15 @@
 
 namespace Njaaazi\Fpl\Facades;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 use Njaaazi\Fpl\FplClient;
 
 /**
  * @see \Njaaazi\Fpl\FplClient
+ * @method static Collection generalInfo()
+ * @method static Collection allFixtures(bool $upcomingFixturesOnly = false)
+ * @method static Collection gameweekFixtures(int $gameweek = 1)
  */
 class Fpl extends Facade
 {
